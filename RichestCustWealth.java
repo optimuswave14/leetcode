0bd2d101f454bb7f0faa77cdca92093fb1653e8f
Deepti <deeptikini2005@gmail.com>
2026-01-30 12:52:12 +0530

    add soln

---
+++ RichestCustWealth.java
@@ -0,0 +1,15 @@
+public class RichestCustWealth {
+    public int maximumWealth(int[][] accounts) {
+        int max=0;
+        for(int i=0;i<accounts.length;i++){
+            int sum=0;
+            for (int j=0;j<accounts[i].length;j++){
+                sum+=accounts[i][j];
+            }
+            if(sum>max){
+                max=sum;
+            }
+        }
+        return max;
+    }
+}
