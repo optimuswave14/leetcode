c9b5ce7cddfe98679f9ddb0fe3268385130c8a60
Deepti <deeptikini2005@gmail.com>
2026-02-01 12:42:14 +0530

    add soln

---
+++ KthLargestIntArr.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class KthLargestIntArr {
+    public String kthLargestNumber(String[] nums, int k) {
+        Arrays.sort(nums, (a, b) -> {
+            if (a.length() != b.length()) {
+                return b.length() - a.length(); 
+            }
+            return b.compareTo(a); 
+        });
+        return nums[k - 1];
+    }
+}
